c2e50cec41a03f2d39177b720d71bfd89eceb69a
PrabhutiSchool <niraj@prabhutisystems.com>
2022-01-13 20:14:52 +0530

    change done branch details

--- SMEnterprise/Models/GeneralModel.cs
+++ SMEnterprise/Models/GeneralModel.cs
@@ -442,6 +442,8 @@ namespace SMEnterprise.Models
         public string BookNo { get; set; }
         public string RenewedUpto { get; set; }
         public int OpType { get; set; }
+        public string UDISECode { get; set; }
+        public string SchoolCode { get; set; }
     }
     public class SubSubjectTypeEditModel
     {
