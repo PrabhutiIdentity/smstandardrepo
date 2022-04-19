425c84dcab7def68fe70e242be200fe466ad18b4
sourabhcse <sourabhsvits@gmail.com>
2022-04-19 19:10:49 +0530

    Admit Card Evaluation Report Section class teacher list Student Page

--- SMEnterprise/Global.asax.cs
+++ SMEnterprise/Global.asax.cs
@@ -107,7 +107,7 @@ namespace SMEnterprise
         {
           //  Session is Available here
             HttpContext context = HttpContext.Current;
-            context.Session["UserID"] = "100";
+          //  context.Session["UserID"] = "100";
 
         }
         protected void Application_PreRequestHandlerExecute(object sender, EventArgs e)
