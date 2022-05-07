77e587b89c168fc04af5e204a9f3eff43d4d6cda
Niraj Kumar <niraj@prabhutisystems.com>
2022-05-07 19:25:39 +0530

    Teacher Incharge Sign update in Report [sp_GetStudentEvaluationPerformanceDetail] Change

--- SMEnterprise/Models/GeneralModel.cs
+++ SMEnterprise/Models/GeneralModel.cs
@@ -449,6 +449,14 @@ namespace SMEnterprise.Models
         public string SessionName { get; set; }
         public string EvaluationName { get; set; }
         //
+        public string TeacherIncharge { get; set; }
+        public string TeacherInchargeSign { get; set; }
+        public int CTeacherID { get; set; }
+        public int InchargeTeacherID { get; set; }
+        
+            
+        public string ClassTeacher { get; set; }
+        public string ClassTeacherSign { get; set; }
     }
     public class SubSubjectTypeEditModel
     {
