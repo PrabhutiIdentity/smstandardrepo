c63855127168e4886dfaca2e91784de590bc69be
Niraj Kumar <niraj@prabhutisystems.com>
2023-04-20 17:28:06 +0530

    Checking shine

--- SMEnterprise/Controllers/AccountController.cs
+++ SMEnterprise/Controllers/AccountController.cs
@@ -592,8 +592,8 @@ namespace SMEnterprise.Controllers
         {
             int iID = CommonUsage.ConvertToInt(ID);
             FeePaymentModel model = await objAccountData.GetFeePaymentReciptDetails(iID);
-            var xmlData = CommonUsage.SerializeToXML<FeePaymentModel>(model);
-            var html = CommonUsage.ConvertToHTML(xmlData, "https://pschoolstorage.blob.core.windows.net/report-formats/PrintFeeReceipt.xslt");
+          //  var xmlData = CommonUsage.SerializeToXML<FeePaymentModel>(model);
+         //   var html = CommonUsage.ConvertToHTML(xmlData, "https://pschoolstorage.blob.core.windows.net/report-formats/PrintFeeReceipt.xslt");
 
             return PartialView("_PrintFeeRecipt", model);
         }
