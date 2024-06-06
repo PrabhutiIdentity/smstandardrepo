dc27434795cb291202285027e3b900d09b542137
Niraj Kumar <niraj@prabhutisystems.com>
2024-06-06 11:49:39 +0530

    update

---
+++ SMEnterprise/Models/EmployeePermissionsModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SMEnterprise.Models
+{
+    public class EmployeePermissionsModel
+    {
+        public int isPermissionsSet { get; set; }
+        public List<EmployeePermissionListModel> Permissions { get; set; }
+    }
+    public class EmployeePermissionListModel
+    {
+        public int Module { get; set; }
+        public int Action { get; set; }
+        public int Status { get; set; }
+    }
+}
