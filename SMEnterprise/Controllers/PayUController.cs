cf596748baec62d7912e6d26760cbc83e2c72d7e
Niraj Kumar <niraj@prabhutisystems.com>
2025-04-11 17:03:55 +0530

    update Payu Payment Gateway

--- SMEnterprise/Controllers/PayUController.cs
+++ SMEnterprise/Controllers/PayUController.cs
@@ -7,11 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using System.Data;
 
 namespace SMEnterprise.Controllers
 {
     public class PayUController : Controller
     {
+        AccountData objAccountData = new AccountData();
         // GET: PayU
         string key = "JVDkAA"; // Your PayU key
         string salt = "MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQC0PewBdO/B+hMc2kbweqBI/0iIBm1FPeSn7wLIQIEQE7jv27XdrFSOt+fDr6Zbp8IZJTUj3eqDw4G6YbQeAfkYy+T/2orcBxsIUbArvWt1QN5GDzP0+v26Mes2fbjCM7sJXYCa4fwYmu4LPDG1X2R0P0Sw3Cjbt1nZRhhZLRw/MrFfJvyyklHU4sP2NgmQxqzdGxGROK3eG3tPMP0nJfVnfFy7VPrM0U11ytJCGalaAiYryeoxXArk/d3zjS9yEXBiRKfQDPFoouoN9PpIGYEw9eLzog2+S9De1cNi3PErBUaiCxlTXF9Bvwc+0gG1t1tBXzo9B1mdjBYnWqZyuWRlAgMBAAECggEAL9tVI5nh4xY8JugJ9+YHUvp9TqiuMLlbUf+TNIpz+knSS9WBd2c/WFmaLwofWKy1S6nmbyyIDQ5HDq7MfT823Y+dsRVc00kLs2zFIwf7VTmxCtcUi5Js9hg1BNXizaWgnr7126nvHrVcSR1lcWvvpzH5UiBStOv87kS/SEZC/sBLnN4Y0Gj51NThj/yB2cRppf51cPd/oCOAfhwZUvFSecQVWESRf2GB0dPnwjGAyB1qFKzICm/o8kqQ6ZyhmssnLuAf3AXXsVSyoZHrDFwZjLtHXA3Lhnqm/ZseCAaj9+dG/V+MRk48HNDV8TWl5JVUTYtDenNMTWm+i+mIysD/jQKBgQDmy9XkjqcELJ6Ss6VKl5MBzHotaJgF9ek3zh1eFmYtRU5nHyqs9bd8htTCcCSNP+yVlQyUJMdTHoUQL7FlK2Nf9Tba0Y33GEih8hwIbmAfhhe7kf6fiA/YDuUmIMuI7d2RDwMSROtdMDi8lQWpnXWJ6WXOWasebt+GUuIqOv7PAwKBgQDH7MiS0IVkI8ax6qlRLN2kaMPp49nNcTDTzaDN2SxVf3V1BhOmvo++M00w71kIXUFjbBtZRI9fMU0qY0/N383MQEgIsUNxfdm7BMk1brGSJTLr/+uc36cYqNWYE9Gldh2W0S5IjjS2hcrs/hpUcVz6NwL0UJlCZTXDwMFyBKcOdwKBgBqmzzxlfMPuoyEtvZiviDpc8n7r5SJLE3NuSdXjoEj6B+PApZzzgwzORSNu78mf6CId5CX3WU0v6Q3FdCbKq98Y4gzCxjISi9CntLUEifUZ9wOiCFVD0RCSJ8QPZXGahkuKAEDL1KIeP28Hhm/fzwLuAOOjVy9cflN0nslhpqshAoGAGZdYsVpZyNC/jWSxWb+5e2MhuN2+soqLqoEG1XK4NxWCroEhoNWBxuIAPoRUzDpLtXNQKOQayh+gdg2SWJMOX9fWoK44KN3oMgVR0DIkLsXuN8FhoooKdKCf/sCCtIBjFzwdhZIWc0q7CA0ax7ZfJXRHP4jVpWcZM76HxJWN4M8CgYBEKYCzLpx2SU0rjsBQAvqGIj/JxTFB4u9qjLEsUiz3WyAidKzOyBwlu00+ZK/mCwlNEVCcG9g0B2xag+sPe3CCF9jomQE14BWiCp1or6G8m1NLGrQSEUy2XAToGwff9XatpTZc6DsRBn7PFUFaxNGM+UcDWYxHWA+JZ2sToYIUhg=="; // Your PayU salt
@@ -21,8 +23,8 @@ namespace SMEnterprise.Controllers
             string transactionId = Guid.NewGuid().ToString("N").Substring(0, 20);
             int StudentID = CommonUsage.ConvertToInt(Session["SChildID"].ToString());
             int SBranchID = 0;
-            AccountData Accountdata = new AccountData();
-            StudentModel studentDetails = Accountdata.GetStudentDetailsForPayment(StudentID, SBranchID);
+           
+            StudentModel studentDetails = objAccountData.GetStudentDetailsForPayment(StudentID, SBranchID);
             string amount1 = oModel.ApplicableFee.ToString("0.00"); // Format as string with two decimal points
             string email1 = string.IsNullOrEmpty(studentDetails.EmailID) ? "info@prabhutisystems.com" : studentDetails.EmailID;
             string phone = string.IsNullOrEmpty(studentDetails.FatherMobileNo) ? "9958803329" : studentDetails.FatherMobileNo;
@@ -31,6 +33,8 @@ namespace SMEnterprise.Controllers
             // string productinfo = "Fee Payment for " + model.Name + "student ID" + model.StudentID;
             string productinfo = "Fee Payment";
             string amount = oModel.ApplicableFee.ToString("0.00");
+
+            
 
             string email = email1;
             var model = new PayUModel
@@ -47,9 +51,13 @@ namespace SMEnterprise.Controllers
                 FailureUrl = Url.Action("PaymentFailed", "PayU", null, Request.Url.Scheme),
                 udf1 = studentDetails.SessionID.ToString(),
                 udf2 = studentDetails.StudentSessionUID.ToString(),
-                udf3 = studentDetails.StudentID.ToString()
+                udf3 = studentDetails.StudentID.ToString(),
+                udf4 = studentDetails.SBranchID.ToString(),
+                udf5 = oModel.FeeMonth.ToString(),
+                udf6 = oModel.FeeYear.ToString()
             };
 
+            // string hashString = model.MerchantKey + "|" + model.TransactionId + "|" + model.Amount + "|" + model.ProductInfo + "|" + model.FirstName + "|" + model.Email + "|" + model.udf1 + "|" + model.udf2 + "|" + model.udf3 + "|" + model.udf4 + "|" + model.udf5 + "|" + model.udf6 + "|||||" + model.Salt;
             string hashString = model.MerchantKey + "|" + model.TransactionId + "|" + model.Amount + "|" + model.ProductInfo + "|" + model.FirstName + "|" + model.Email + "|" + model.udf1 + "|" + model.udf2 + "|" + model.udf3 + "||||||||" + model.Salt;
             model.Hash = GenerateHash512(hashString);
             OrderModel orderModel = new OrderModel
@@ -70,10 +78,13 @@ namespace SMEnterprise.Controllers
                 StudentID = studentDetails.StudentID,
                 SBranchID = studentDetails.SBranchID,
                 SessionID = studentDetails.SessionID,
+                
 
             };
+            
             orderModel.Date = SMEnterprise.Repository.CommonUsage.GetCurrentDate();
-            Accountdata.InsertOrderID(orderModel);
+            objAccountData.InsertOrderID(orderModel);
+            
             return View(model);
         }
 
@@ -105,6 +116,9 @@ namespace SMEnterprise.Controllers
             string sessionId = form["udf1"];
             string studentSessionId = form["udf2"];
             string studentId = form["udf3"];
+            string sbranchId = form["udf4"];
+            string feeMonth = form["udf5"];
+            string feeYear = form["udf6"];
             string bank_ref_num = form["bank_ref_num"];
 
             // Handle Additional Charges if Present
@@ -133,6 +147,20 @@ namespace SMEnterprise.Controllers
                 ViewBag.PaymentId = mihpayid;
                 ViewBag.Amount = amount;
                 ViewBag.TransactionId = txnid;
+                FeePaymentModel objModel=new FeePaymentModel();
+
+                objModel.SBranchID = CommonUsage.ConvertToInt(sbranchId);
+                objModel.StudentID = CommonUsage.ConvertToInt(studentId);
+                objModel.SessionID = CommonUsage.ConvertToInt(sessionId);
+                objModel.FeeAmount = CommonUsage.ConvertToDecimal(amount);
+                objModel.FeePaymentMode = 3;
+                objModel.PaymentAmount = CommonUsage.ConvertToDecimal(amount);
+                objModel.ReferanceNumber = bank_ref_num;
+                objModel.Remark = "Paid by PayU PaymentGateway";
+                objModel.Month = CommonUsage.ConvertToInt(feeMonth);
+                objModel.Year = CommonUsage.ConvertToInt(feeYear);
+                objAccountData.UpdateOrderStatus(txnid,studentId,sessionId,mihpayid);
+                FeePaymentRowModel objData = objAccountData.SaveStudentFeePaymentOnline(objModel);
             }
             else
             {
@@ -140,7 +168,7 @@ namespace SMEnterprise.Controllers
                 ViewBag.Message = "Payment verification failed!";
             }
 
-            return View();
+            return View("FeePayment");
         }
 
         public ActionResult PaymentFailed()
@@ -168,17 +196,26 @@ namespace SMEnterprise.Controllers
         public string udf1 { get; set; }
         public string udf2 { get; set; }
         public string udf3 { get; set; }
+        public string udf4 { get; set; }
+        public string udf5 { get; set; }
+        public string udf6 { get; set; }
         public string MerchantKey { get; set; }
         public string Salt { get; set; }
         public string TransactionId { get; set; }
         public string Amount { get; set; }
         public string ProductInfo { get; set; }
         public string FirstName { get; set; }
+      
         public string Email { get; set; }
         public string Phone { get; set; }
         public string SuccessUrl { get; set; }
         public string FailureUrl { get; set; }
         public string Hash { get; set; }
+        public int StudentID { get; set; }
+        public int FeeMonth { get; set; }
+        public int FeeYear { get; set; }
+
+
     }
 
 }
